f9b4be70d988269ec54f7830d5422d0ef719fc63
Joffrey Bion <joffrey.bion@booking.com>
2020-04-03 23:43:41 +0200

    Add plugin parameters for directory support
    Resolves:
    https://github.com/kobylynskyi/graphql-java-codegen/issues/30

---
+++ plugins/graphql-java-codegen-maven-plugin/src/main/java/io/github/kobylynskyi/graphql/codegen/SchemaFinderConfig.java
@@ -0,0 +1,49 @@
+package io.github.kobylynskyi.graphql.codegen;
+
+import com.kobylynskyi.graphql.codegen.supplier.SchemaFinder;
+
+import java.util.Collections;
+import java.util.Set;
+
+public class SchemaFinderConfig {
+
+    private String rootDir;
+
+    private boolean recursive = SchemaFinder.DEFAULT_RECURSIVE;
+
+    private String includePattern = SchemaFinder.DEFAULT_INCLUDE_PATTERN;
+
+    private Set<String> excludedFiles = Collections.emptySet();
+
+    public String getRootDir() {
+        return rootDir;
+    }
+
+    public void setRootDir(String rootDir) {
+        this.rootDir = rootDir;
+    }
+
+    public boolean isRecursive() {
+        return recursive;
+    }
+
+    public void setRecursive(boolean recursive) {
+        this.recursive = recursive;
+    }
+
+    public String getIncludePattern() {
+        return includePattern;
+    }
+
+    public void setIncludePattern(String includePattern) {
+        this.includePattern = includePattern;
+    }
+
+    public Set<String> getExcludedFiles() {
+        return excludedFiles;
+    }
+
+    public void setExcludedFiles(Set<String> excludedFiles) {
+        this.excludedFiles = excludedFiles;
+    }
+}
