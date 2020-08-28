d8f0907b9e057234057af8691c5077ebc7e4b029
梦境迷离 <dreamylost@outlook.com>
2020-08-28 13:21:28 +0800

    fix comments

--- src/main/java/com/kobylynskyi/graphql/codegen/model/graphql/GraphQLResponseProjection.java
+++ src/main/java/com/kobylynskyi/graphql/codegen/model/graphql/GraphQLResponseProjection.java
@@ -11,24 +11,23 @@ public abstract class GraphQLResponseProjection {
     protected final List<GraphQLResponseField> fields = new ArrayList<>();
 
     /**
-     * save current depth for self recursive type.
+     * save current depth for self recursive type.(it's actually a marker)
      * such as
      * {{@code
      * type Human implements Character {
      *     id: ID!
      *     friends: [Character] # if you response this field on Human projection , Character has itself,
-     *     # so, we need know depth of subquery
+     *     # so, we need know depth of subquery.
      * }
      * interface Character {
      *     id: ID!
      *     friends: [Character]
      * }
-     *
-     * Note:
-     * Map key is parentProjection.childProjection.currentMethod. e.g. CharacterResponseProjection.CharacterResponseProjection.friends (excluding the first layer, so if only want the first child layer, use `all$(1)`)
-     * Map value is current depth for Character type. Values exists 1 or 0, because each projection have a new instance of projectionDepthOnFields, so it always be `1`
-     * and `responseProjectionMaxDepth` will reduce by recursive. it's actually a marker.
      * }}
+     * Map Notes:
+     * `key` is parentProjection.childProjection.currentMethod. e.g. `CharacterResponseProjection.CharacterResponseProjection.friends` (excluding the first layer, so if only want the first child layer, use `all$(1)`)
+     * `value` is current depth for Character type. Each projection has a new instance of `projectionDepthOnFields`, so it always be `1` or `0`.
+     * and `responseProjectionMaxDepth` will reduce by recursive.
      */
     protected final Map<String, Integer> projectionDepthOnFields = new HashMap<>();
 
