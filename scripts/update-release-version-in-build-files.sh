0d48f086c29f6d19d3359e0292be399fb2f355cc
Bogdan Kobylynskyi <92bogdan@gmail.com>
2021-03-04 19:38:19 -0500

    Fix release workflow

--- scripts/update-release-version-in-build-files.sh
+++ scripts/update-release-version-in-build-files.sh
@@ -4,13 +4,17 @@ RELEASE_VERSION=$1
 RELEASE_VERSION_ESCAPED=${RELEASE_VERSION//./\\.}
 
 set_version_in_file() {
-  sed -i '' "s/$2[A-Z0-9.\-]*/$2$RELEASE_VERSION_ESCAPED/" "$1"
+  # for mac add '' after -i: sed -i '' "s...
+  sed -i "s/$2[A-Z0-9.\-]*/$2$RELEASE_VERSION_ESCAPED/gw /tmp/sed.done" "$1"
   echo "Updated version in $1"
+  grep --color -e "" /tmp/sed.done
 }
 
-set_version_in_file "build.gradle" "var graphqlCodegenVersion = '"
+set_version_in_file "build.gradle" "def graphqlCodegenVersion = '"
 
-set_version_in_file "plugins/gradle/graphql-java-codegen-gradle-plugin/build.gradle" "var graphqlCodegenGradlePluginVersion = '"
+set_version_in_file "plugins/maven/graphql-java-codegen-maven-plugin/pom.xml" "<version.graphql-java-codegen>"
+
+set_version_in_file "plugins/gradle/graphql-java-codegen-gradle-plugin/build.gradle" "def graphqlCodegenGradlePluginVersion = '"
 
 set_version_in_file "plugins/gradle/example-server/build.gradle" "io.github.kobylynskyi.graphql.codegen\" version \""
 
@@ -18,7 +22,7 @@ set_version_in_file "plugins/gradle/example-client/build.gradle" "implementation
 set_version_in_file "plugins/gradle/example-client/build.gradle" "io.github.kobylynskyi.graphql.codegen\" version \""
 
 set_version_in_file "plugins/gradle/example-client-kotlin/build.gradle" "id \"io.github.kobylynskyi.graphql.codegen\" version \""
-set_version_in_file "plugins/gradle/example-client-kotlin/build.gradle" "var graphqlCodegenClientKotlinVersion = '"
+set_version_in_file "plugins/gradle/example-client-kotlin/build.gradle" "def graphqlCodegenClientKotlinVersion = '"
 set_version_in_file "plugins/gradle/example-client-kotlin/build.gradle" "implementation \"io.github.kobylynskyi:graphql-java-codegen:"
 
 set_version_in_file "plugins/sbt/graphql-java-codegen-sbt-plugin/version.sbt" "version in ThisBuild := \""
