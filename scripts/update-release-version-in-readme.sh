c7255c2b7e8a43033e6583457838f83dc5704155
Bogdan Kobylynskyi <92bogdan@gmail.com>
2021-03-04 19:38:19 -0500

    Fix release workflow

--- scripts/update-release-version-in-readme.sh
+++ scripts/update-release-version-in-readme.sh
@@ -4,8 +4,9 @@ RELEASE_VERSION=$1
 RELEASE_VERSION_ESCAPED=${RELEASE_VERSION//./\\.}
 
 set_version_in_file() {
-  sed -i '' "s/$2[A-Z0-9.\-]*/$2$RELEASE_VERSION_ESCAPED/" "$1"
+  sed -i "s/$2[A-Z0-9.\-]*/$2$RELEASE_VERSION_ESCAPED/gw /tmp/sed.done" "$1"
   echo "Updated version in $1"
+  grep --color -e "" /tmp/sed.done
 }
 
 set_version_in_file "plugins/gradle/README.md" "id \"io.github.kobylynskyi.graphql.codegen\" version \""
