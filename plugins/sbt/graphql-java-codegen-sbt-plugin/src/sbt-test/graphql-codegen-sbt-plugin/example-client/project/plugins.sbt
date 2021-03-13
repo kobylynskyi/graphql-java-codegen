51f348cb56c2fb42c5e11470e9573b4ea6def752
Bogdan Kobylynskyi <92bogdan@gmail.com>
2021-03-12 21:38:57 -0600

    Bump to release version 4.1.5

--- plugins/sbt/graphql-java-codegen-sbt-plugin/src/sbt-test/graphql-codegen-sbt-plugin/example-client/project/plugins.sbt
+++ plugins/sbt/graphql-java-codegen-sbt-plugin/src/sbt-test/graphql-codegen-sbt-plugin/example-client/project/plugins.sbt
@@ -1,1 +1,1 @@
-addSbtPlugin("io.github.jxnu-liguobin" % "graphql-codegen-sbt-plugin" % "4.1.5-SNAPSHOT")
+addSbtPlugin("io.github.jxnu-liguobin" % "graphql-codegen-sbt-plugin" % "4.1.5")
--- scripts/update-release-version-in-readme.sh
+++ scripts/update-release-version-in-readme.sh
@@ -4,7 +4,7 @@ RELEASE_VERSION=$1
 RELEASE_VERSION_ESCAPED=${RELEASE_VERSION//./\\.}
 
 set_version_in_file() {
-  sed -i "s/$2[A-Z0-9.\-]*/$2$RELEASE_VERSION_ESCAPED/gw /tmp/sed.done" "$1"
+  sed -i '' "s/$2[A-Z0-9.\-]*/$2$RELEASE_VERSION_ESCAPED/gw /tmp/sed.done" "$1"
   echo "Updated version in $1"
   grep --color -e "" /tmp/sed.done
 }
