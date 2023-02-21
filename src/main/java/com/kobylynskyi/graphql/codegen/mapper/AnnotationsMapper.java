f60d4b78fd1b4fbcd976b0bc327c709d800887e7
esfomeado <vps1992@live.com.pt>
2023-02-21 20:16:57 +0000

    Generate Jackson annotations for interfaces #1033 #1034

--- src/main/java/com/kobylynskyi/graphql/codegen/mapper/AnnotationsMapper.java
+++ src/main/java/com/kobylynskyi/graphql/codegen/mapper/AnnotationsMapper.java
@@ -8,6 +8,7 @@ import com.kobylynskyi.graphql.codegen.utils.Utils;
 import graphql.language.Argument;
 import graphql.language.Directive;
 import graphql.language.InputValueDefinition;
+import graphql.language.InterfaceTypeDefinition;
 import graphql.language.ListType;
 import graphql.language.NamedNode;
 import graphql.language.NonNullType;
@@ -204,7 +205,7 @@ public abstract class AnnotationsMapper {
     public List<String> getJacksonTypeIdAnnotations(MappingContext mappingContext, NamedNode<?> def) {
         List<String> defaults = new ArrayList<>();
         if (Boolean.TRUE.equals(mappingContext.getGenerateJacksonTypeIdResolver())
-                && def instanceof UnionTypeDefinition) {
+                && (def instanceof UnionTypeDefinition || def instanceof InterfaceTypeDefinition)) {
             defaults.add("com.fasterxml.jackson.annotation.JsonTypeInfo(use = " +
                     "com.fasterxml.jackson.annotation.JsonTypeInfo.Id.NAME, property = \"__typename\")");
             String modelPackageName = DataModelMapper.getModelPackageName(mappingContext);
